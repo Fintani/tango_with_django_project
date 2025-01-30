acd03a17f6187663dfa8a5ba530113cbfdb1f22c
Fintan Irving <2890944i@student.gla.ac.uk>
2025-01-30 17:22:25 +0000

    Chapter 3 Excersises Incomplete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for tango_with_django_project project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
