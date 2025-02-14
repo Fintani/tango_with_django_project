fca7e9d8b6c0818a7cb8d4f578081f0f44ae06f4
Fintan Irving <2890944i@student.gla.ac.uk>
2025-02-14 15:21:48 +0000

    Chapter 10 Exercises Complete + Fixed visitor_cookie_handler()

--- rango/views.py
+++ rango/views.py
@@ -41,13 +41,17 @@ def index(request):
     context_dict["pages"] = page_list
 
     visitor_cookie_handler(request)
-    context_dict["visits"] = request.session["visits"]
 
     response = render(request, "rango/index.html", context=context_dict)
     return response
 
 def about(request):
-    return render(request, "rango/about.html")
+    context_dict={}
+
+    visitor_cookie_handler(request)
+    context_dict["visits"] = request.session["visits"]
+
+    return render(request, "rango/about.html", context=context_dict)
 
 @login_required
 def add_category(request):
@@ -164,12 +168,12 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
 
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get("visists", "1"))
+    visits = int(get_server_side_cookie(request,"visits", "1"))
     last_visit_cookie = get_server_side_cookie(request,
                                                "last_visit",
                                                str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        "%y-%m-%d %H:%M:%S")
+                                        "%Y-%m-%d %H:%M:%S")
     
     if (datetime.now()-last_visit_time).days > 0:
         visits = visits +1
